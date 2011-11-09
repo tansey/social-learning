b1b0ddae7b9c4fc52b7ca9f6480ad88740ee969b
Wesley <wes.tansey@gmail.com>
2011-11-08 21:44:46 -0600

    Added SharpNEAT 2 source

---
+++ social_learning/NeuralAgent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpNeat.Phenomes;
+
+namespace social_learning
+{
+    public class NeuralAgent : Agent
+    {
+        public IBlackBox Brain { get; set; }
+        
+        public NeuralAgent(IBlackBox brain)
+        {
+            Brain = brain;
+        }
+
+        protected override float getRotation(double[] sensors)
+        {
+            // Convert the sensors into an input array for the network
+            for (int i = 0; i < sensors.Length; i++)
+                Brain.InputSignalArray[i] = sensors[i];
+
+            // Activate the network
+            Brain.Activate();
+
+            // TODO: Something based on what the output of the neural network tells us
+            var output = Brain.OutputSignalArray[0];
+
+            // [0,1] -> [-180,180]
+            var result = (float)(output-0.5) * 360;
+
+            //MessageBox.Show(result.ToString());
+
+            return result;
+        }
+
+        public override void Reset()
+        {
+            Brain.ResetState();
+        }
+    }
+}
