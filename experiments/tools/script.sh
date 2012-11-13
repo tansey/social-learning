60787c0f7cc771fa8c36520dc82ca7cd038e6bef
Eliana Feasley <e.feasley@gmail.com>
2012-11-12 19:18:35 -0600

    Updated xml writer

--- experiments/tools/script.sh
+++ experiments/tools/script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 gens=250;
-learns=("Neuro" "Social" "Angry");
-switchs=(0 1);
+learns=("Neural" "SubcultureRewards" "SubculturePolling");
+
+switchs=(0 1 10 50);
 runs=({1..2});
 job=0
 root="/u/elie/condor_experiments/"
@@ -16,6 +17,7 @@ do
 				mkdir $root$learn"_"$switch"_"$run
 				python write_xml.py    $root$learn"_"$switch"_"$run $learn $switch
 				python write_condor.py $root$learn"_"$switch"_"$run/ /u/elie/condorapp/CondorApp.exe $job $gens $run
+				echo condor_submit "condor_job"$job".txt"
 				job=`expr $job + 1` 
 
 		done
--- experiments/tools/write_xml.py
+++ experiments/tools/write_xml.py
@@ -7,66 +7,81 @@ def main(args):
 	learn= args[2]
 	switch = args[3]
 
-
 	f = open(init_dir+"config.xml","w")
 
+	if learn == "Neural":
+		agent_type="Neural"
+	else:
+		agent_type="Social"
+
 	f.write("""<?xml version="1.0" encoding="utf-8" ?>
-	<Config>
-	  <PopulationSize>100</PopulationSize>
-	  <SpecieCount>10</SpecieCount>
-	  <Activation>
-	    <Scheme>CyclicFixedIters</Scheme>
-	    <Iters>4</Iters>
-	  </Activation>
-	  <ComplexityRegulationStrategy>Absolute</ComplexityRegulationStrategy>
-	  <ComplexityThreshold>50</ComplexityThreshold>
-	  <AgentType>Neural</AgentType>
-	  <PlantLayout>Uniform</PlantLayout>
-	  <EvolutionParadigm>          %s              </EvolutionParadigm>
-	  <Description>
-	    Foraging Evolution
-	    Fitness is the sum of the scores of the plants an individual ate.
-	  </Description>
-	  <TimeStepsPerGeneration>1000</TimeStepsPerGeneration>
-	  <PlantSpecies>
-	    <Plant>
-	      <Name>Poison_1</Name>
-	      <Radius>5</Radius>
-	      <Reward>-100</Reward>
-	      <Count>20</Count>
-	    </Plant>
-	    <Plant>
-	      <Name>Poison_2</Name>
-	      <Radius>5</Radius>
-	      <Reward>-50</Reward>
-	      <Count>20</Count>
-	    </Plant>
-	    <Plant>
-	      <Name>Neutral_1</Name>
-	      <Radius>5</Radius>
-	      <Reward>0</Reward>
-	      <Count>20</Count>
-	    </Plant>
-	    <Plant>
-	      <Name>Nutritious_1</Name>
-	      <Radius>5</Radius>
-	      <Reward>100</Reward>
-	      <Count>20</Count>
-	    </Plant>
-	    <Plant>
-	      <Name>Nutritious_2</Name>
-	      <Radius>5</Radius>
-	      <Reward>50</Reward>
-	      <Count>20</Count>
-	    </Plant>
-	  </PlantSpecies>
-	  <Predators>3</Predators>
-	  <WorldWidth>500</WorldWidth>
-	  <WorldHeight>500</WorldHeight>
-	  <AgentHorizon>100</AgentHorizon>
-	  <AgentsNavigate>True</AgentsNavigate>
-	  <AgentsHide>True</AgentsHide>
-	  <StepReward>1</StepReward>
-	</Config>""" %(learn))
+<Config>
+  <PopulationSize>100</PopulationSize>
+  <SpecieCount>10</SpecieCount>
+  <Activation>
+    <Scheme>CyclicFixedIters</Scheme>
+    <Iters>4</Iters>
+  </Activation>
+  <ComplexityRegulationStrategy>Absolute</ComplexityRegulationStrategy>
+  <ComplexityThreshold>50</ComplexityThreshold>
+  <AgentType>%s</AgentType>
+  <Memory>
+    <Paradigm>Fixed</Paradigm>
+    <GrowthGenerations>20</GrowthGenerations>
+    <Size>1</Size>
+    <MaxSize>1</MaxSize>
+  </Memory>
+  <TeachingParadigm>%s</TeachingParadigm>
+  <PlantLayout>Uniform</PlantLayout>
+  <EvolutionParadigm>Darwinian</EvolutionParadigm>
+  <Description>
+    Foraging Evolution
+ 
+    Fitness is the sum of the scores of the plants an individual ate minus the predator penalties.
+  </Description>
+  <TimeStepsPerGeneration>1000</TimeStepsPerGeneration>
+  <PlantSpecies>
+    <Plant>
+      <Name>Poison_1</Name>
+      <Radius>5</Radius>
+      <Reward>-100</Reward>
+      <Count>20</Count>
+    </Plant>
+    <Plant>
+      <Name>Poison_2</Name>
+      <Radius>5</Radius>
+      <Reward>-50</Reward>
+      <Count>20</Count>
+    </Plant>
+    <Plant>
+      <Name>Neutral_1</Name>
+      <Radius>5</Radius>
+      <Reward>0</Reward>
+      <Count>20</Count>
+    </Plant>
+    <Plant>
+      <Name>Nutritious_1</Name>
+      <Radius>5</Radius>
+      <Reward>100</Reward>
+      <Count>20</Count>
+    </Plant>
+    <Plant>
+      <Name>Nutritious_2</Name>
+      <Radius>5</Radius>
+      <Reward>50</Reward>
+      <Count>20</Count>
+    </Plant>
+  </PlantSpecies>
+  <Predators>10</Predators>
+  <PredatorTypes>2</PredatorTypes>
+  <PredatorDistribution>Alternating</PredatorDistribution>
+  <PredatorGenerations>%s</PredatorGenerations>
+  <WorldWidth>500</WorldWidth>
+  <WorldHeight>500</WorldHeight>
+  <AgentHorizon>100</AgentHorizon>
+  <AgentsNavigate>True</AgentsNavigate>
+  <AgentsHide>True</AgentsHide>
+  <StepReward>0</StepReward>
+</Config>""" %(social, learn, switch))
 
 main(sys.argv)
